928c91ab0f928e263ca5cbc5d086d163538373cf
Marcelo Zilio Correa <marcelo.correa@southsystem.com.br>
2019-04-04 08:48:50 -0300

    Limpando conteúdo retornado pelo wikipedia

--- robots/search.js
+++ robots/search.js
@@ -10,9 +10,30 @@ async function robot(question) {
                 .algo('web/WikipediaParser/0.1.2')
                 .pipe({ "articleName": question, "lang": "pt" })
                 .then(function (response) {
-                    resolve(response.get().content)
+                    resolve(sanitizeContent(response.get().content))
                 })
         })
+    }
+
+    function sanitizeContent(text) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(text)
+        return removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                    return false
+                }
+                return true
+            })
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+
+        function removeDatesInParentheses(text) {
+            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
+        }
     }
 }
 
